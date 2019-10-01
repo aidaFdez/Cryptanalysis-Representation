7fdca2e797a4fa3505648223ce30fac7f163a9d0
aidaFdez <33495958+aidaFdez@users.noreply.github.com>
2019-10-01 16:10:32 +0100

    Began the GUI

--- GUI.py
+++ GUI.py
@@ -1,1 +1,17 @@
+from tkinter import *
+from tkinter.ttk import *
 
+window =Tk()
+window.title("Cryptanalysis")
+
+#GUI for choosing the kind of cryptanalysis
+messKind = Label(window, text="Choose the kind of cryptanalysis")
+messKind.grid(column = 0, row  = 0)
+linear = Radiobutton(window, text ="Linear", value = 1)
+differential = Radiobutton(window, text ="Differential", value = 2)
+differential.grid(column = 0, row = 1)
+linear.grid(column = 0, row = 2)
+
+
+
+window.mainloop()
